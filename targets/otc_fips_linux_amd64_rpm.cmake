33c84931ec72c82ff7c9e795747f2fe8b9f222aa
Justin Kolberg <amd.prophet@gmail.com>
2024-08-06 16:07:53 -0700

    feat: add fips targets for linux/amd64 & linux/arm64
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

---
+++ targets/otc_fips_linux_amd64_rpm.cmake
@@ -0,0 +1,16 @@
+set(package_name "otelcol-sumo-fips")
+set(package_os "linux")
+set(package_arch "x86_64")
+set(goos "linux")
+set(goarch "amd64")
+set(fips 1)
+
+set_common_settings()
+set_otc_settings()
+set_common_rpm_settings()
+set_otc_rpm_settings()
+
+default_otc_linux_install()
+install_otc_service_systemd()
+
+build_rpm_cpack_config()
