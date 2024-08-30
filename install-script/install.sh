2ec9db379a1cb74b31212f2f2bbf64bcb77e1d25
Eric Chlebek <echlebek@sumologic.com>
2024-08-30 16:05:06 -0700

    Use more --quiet
    Signed-off-by: Eric Chlebek <echlebek@sumologic.com>

--- install-script/install.sh
+++ install-script/install.sh
@@ -1092,13 +1092,13 @@ function install_linux_package() {
     case $(get_package_manager) in
         yum | dnf)
             curl -s https://packagecloud.io/install/repositories/sumologic/stable/script.rpm.sh | bash
-            yum --disablerepo="*" --enablerepo="sumologic_stable" -y update
-            yum install "${package_with_version}"
+            yum --quiet --disablerepo="*" --enablerepo="sumologic_stable" -y update
+            yum install --quiet "${package_with_version}"
             ;;
         apt-get)
             curl -s https://packagecloud.io/install/repositories/sumologic/stable/script.deb.sh | bash
-            apt-get update -y -o Dir::Etc::sourcelist="sources.list.d/sumologic_stable"
-            apt-get install "${package_with_version}"
+            apt-get update --quiet -y -o Dir::Etc::sourcelist="sources.list.d/sumologic_stable"
+            apt-get install --quiet "${package_with_version}"
             ;;
     esac
 }
