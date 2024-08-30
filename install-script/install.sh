be149ca97e466a5f2f99f21fd14a679c8e454cc5
Eric Chlebek <echlebek@sumologic.com>
2024-08-30 16:03:02 -0700

    Fix crash when otelcol-config is not yet installed
    Signed-off-by: Eric Chlebek <echlebek@sumologic.com>

--- install-script/install.sh
+++ install-script/install.sh
@@ -1151,7 +1151,9 @@ if [[ "${UNINSTALL}" == "true" ]]; then
 fi
 
 # Attempt to find a token from an existing installation
-USER_TOKEN=$(otelcol-config --read-kv .extensions.sumologic.installation_token)
+if command -v otelcol-config &> /dev/null; then
+    USER_TOKEN=$(otelcol-config --read-kv .extensions.sumologic.installation_token)
+fi
 if [[ -z "${USER_TOKEN}" ]]; then
     USER_TOKEN="$(get_user_env_config "${TOKEN_ENV_FILE}")"
 fi
