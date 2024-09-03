1162d9734d52e62625b8cfc9a9c9162bac8fb904
Eric Chlebek <echlebek@sumologic.com>
2024-09-03 11:28:07 -0700

    Expect different permissions
    Signed-off-by: Eric Chlebek <echlebek@sumologic.com>

--- install-script/test/consts_darwin.go
+++ install-script/test/consts_darwin.go
@@ -4,17 +4,17 @@ const (
 	appSupportDirPath          string = "/Library/Application Support/otelcol-sumo"
 	packageName                string = "otelcol-sumo.pkg"
 	launchdPath                string = "/Library/LaunchDaemons/com.sumologic.otelcol-sumo.plist"
-	launchdPathFilePermissions uint32 = 0640
+	launchdPathFilePermissions uint32 = 0600
 	uninstallScriptPath        string = appSupportDirPath + "/uninstall.sh"
 
 	// TODO: fix mismatch between darwin permissions & linux binary install permissions
 	// 00-otelcol-config-settings.yaml must be writable as the install scripts mutate it
-	commonConfigPathFilePermissions uint32 = 0660
-	configPathDirPermissions        uint32 = 0770
-	configPathFilePermissions       uint32 = 0440
-	confDPathFilePermissions        uint32 = 0644
+	commonConfigPathFilePermissions uint32 = 0600
+	configPathDirPermissions        uint32 = 0700
+	configPathFilePermissions       uint32 = 0600
+	confDPathFilePermissions        uint32 = 0600
 	etcPathPermissions              uint32 = 0751
-	opampDPermissions               uint32 = 0750
+	opampDPermissions               uint32 = 0700
 
 	rootGroup   string = "wheel"
 	rootUser    string = "root"
