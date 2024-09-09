03c23f0b5b942026a6b795bab3db930f69805e74
Eric Chlebek <echlebek@sumologic.com>
2024-09-09 16:41:09 -0700

    Change expected Linux perms
    Signed-off-by: Eric Chlebek <echlebek@sumologic.com>

--- install-script/test/consts_linux.go
+++ install-script/test/consts_linux.go
@@ -7,10 +7,10 @@ const (
 	// TODO: fix mismatch between package permissions & expected permissions
 	commonConfigPathFilePermissions uint32 = 0750
 	configPathDirPermissions        uint32 = 0770
-	configPathFilePermissions       uint32 = 0440
-	confDPathFilePermissions        uint32 = 0644
+	configPathFilePermissions       uint32 = 0600
+	confDPathFilePermissions        uint32 = 0600
 	etcPathPermissions              uint32 = 0551
-	opampDPermissions               uint32 = 0750
+	opampDPermissions               uint32 = 0770
 
 	rootGroup   string = "root"
 	rootUser    string = "root"
