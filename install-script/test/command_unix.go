6f4620b2903388aa6a242e00b4a308bbfe04fcbc
Justin Kolberg <amd.prophet@gmail.com>
2024-10-04 15:59:56 -0700

    remove condition for now
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- install-script/test/command_unix.go
+++ install-script/test/command_unix.go
@@ -93,7 +93,8 @@ func (io *installOptions) string() []string {
 
 	if io.version != "" {
 		opts = append(opts, "--version", io.version)
-	} else if otc_build_number != "" {
+		//} else if otc_version != "" && otc_build_number != "" {
+	} else {
 		opts = append(opts, "--version", fmt.Sprintf("%s-%s", otc_version, otc_build_number))
 	}
 
