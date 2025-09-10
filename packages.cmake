6ebe53fb5d7616a82806f0592e6a2e4e8e44881f
Astitva Sood <astitva.sood@sumologic.com>
2025-09-10 19:06:04 +0530

    ci: adding second s3 target to upload ci builds to

--- packages.cmake
+++ packages.cmake
@@ -71,7 +71,7 @@ macro(build_cpack_config)
   # Add a target for uploading the package to Amazon S3
   set(_version "${OTC_VERSION}-${BUILD_NUMBER}")
   set(_s3_path "${_version}/")
-  create_s3_cp_target(${_s3_bucket} ${_s3_path} ${_package_output})
+  create_s3_cp_target("sumologic-osc-ci" ${_s3_path} ${_package_output})
   create_s3_cp_target_new("sumo-otel-builds-dev-c64ec98a" ${_s3_path} ${_package_output})
 
   # Add a publish-package target to publish the package built above
