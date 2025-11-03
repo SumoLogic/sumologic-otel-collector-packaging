06f326403992eb0b1584cc43df207dd83acaceeb
Shubham Gupta <shubham.gupta@sumologic.com>
2025-11-03 16:36:31 +0530

    SUMO-272481 add windows test for clobber

--- msi/SumoLogic.wixext/SumoLogicTests/ConfigUpdaterTests.cs
+++ msi/SumoLogic.wixext/SumoLogicTests/ConfigUpdaterTests.cs
@@ -187,6 +187,27 @@ namespace SumoLogicTests
             Assert.AreEqual(config.Timezone, endpoint.ToString());
         }
 
+        public void ClobberAssertions(Config config, StreamReader sr)
+        {
+            YamlStream ys = new YamlStream();
+            ys.Load(sr);
+            YamlMappingNode root = (YamlMappingNode)ys.Documents[0].RootNode;
+
+            Assert.IsTrue(root.Children.ContainsKey("extensions"));
+            Assert.AreEqual(YamlNodeType.Mapping, root.Children["extensions"].NodeType);
+            var extensions = (YamlMappingNode)root.Children["extensions"];
+
+            Assert.IsTrue(extensions.Children.ContainsKey("sumologic"));
+            Assert.AreEqual(YamlNodeType.Mapping, extensions.Children["sumologic"].NodeType);
+            var sumologic = (YamlMappingNode)extensions.Children["sumologic"];
+
+            Assert.IsTrue(sumologic.Children.ContainsKey("clobber"));
+            Assert.AreEqual(YamlNodeType.Scalar, sumologic.Children["clobber"].NodeType);
+            var endpoint = (YamlScalarNode)sumologic.Children["clobber"];
+            Assert.AreEqual(config.Clobber, endpoint.ToString());
+        }
+
+
 
         [TestMethod]
         public void TestUpdate_WithExtensionsBlock()
@@ -500,6 +521,33 @@ namespace SumoLogicTests
                 ms.Seek(0, SeekOrigin.Begin);
 
                 TimezoneAssertions(config, sr);
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdate_Clobber()
+        {
+            var filePath = Path.Combine(testDataPath, "with-extensions-block.yaml");
+            var config = new Config { InstallationToken = "foobar", Clobber = "true" };
+            config.SetCollectorFieldsFromTags(@"foo=bar,baz=kaz,xaz=yaz");
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var configUpdater = new ConfigUpdater(new StreamReader(filePath));
+                configUpdater.Update(config);
+                configUpdater.Save(new StreamWriter(ms));
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                StreamReader sr = new StreamReader(ms);
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                ClobberAssertions(config, sr);
             }
         }
 
