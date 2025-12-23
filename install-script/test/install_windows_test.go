40267bb677b37302e9b38f2e5a609696d8384038
Shubham Gupta <shubham.gupta@sumologic.com>
2025-12-23 17:13:40 +0530

    feat: reduce installed checks

--- install-script/test/install_windows_test.go
+++ install-script/test/install_windows_test.go
@@ -15,16 +15,6 @@ func TestInstallScript(t *testing.T) {
 		checkConfigNotCreated,
 		checkUserConfigNotCreated,
 	}
-    installedChecks := []checkFunc{
-    				checkBinaryCreated,
-    				checkBinaryIsRunning,
-    				checkConfigCreated,
-    				checkConfigFilesOwnershipAndPermissions(localSystemSID),
-    				checkUserConfigCreated,
-    				checkEphemeralNotInConfig(userConfigPath),
-    				checkTokenInConfig,
-    				checkHostmetricsConfigNotCreated,
-    }
 	for _, spec := range []testSpec{
 		{
 			name:        "no arguments",
@@ -205,7 +195,11 @@ func TestInstallScript(t *testing.T) {
 				packagePath:  getPackagePath(t),
 			},
 			preChecks: notInstalledChecks,
-			postChecks: installedChecks,
+			postChecks: []checkFunc{
+				checkBinaryCreated,
+				checkBinaryIsRunning,
+				checkConfigCreated,
+			},
 		},
 	} {
 		t.Run(spec.name, func(t *testing.T) {
