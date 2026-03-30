447ccdd449dedce83fe300348a84f508f3d76d4b
Raunak Mandaokar <rmandaokar@sumologic.com>
2026-03-30 18:26:40 +0530

    chore: added additional md files for chef

--- examples/chef/sumologic-otel-collector/metadata.rb
+++ examples/chef/sumologic-otel-collector/metadata.rb
@@ -1,13 +1,13 @@
 name 'sumologic-otel-collector'
 maintainer 'Sumo Logic'
 maintainer_email 'collection@sumologic.com'
-issues_url 'https://github.com/SumoLogic/sumologic-otel-collector-packaging/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/SumoLogic/sumologic-otel-collector-packaging/tree/main/examples/chef' if respond_to?(:source_url)
+issues_url 'https://github.com/SumoLogic/sumologic-otel-collector-packaging/issues'
+source_url 'https://github.com/SumoLogic/sumologic-otel-collector-packaging/tree/main/examples/chef'
 license 'Apache-2.0'
 description 'Installs sumologic-otel-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
-chef_version '>= 11' if respond_to?(:chef_version)
+chef_version '>= 16'
 
 %w[
   debian
